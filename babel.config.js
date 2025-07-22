e2efa67336f559d4667da987ca14f216ec95fd4d
Lynnn8 <127668009+Lynnn8@users.noreply.github.com>
2025-07-22 21:07:41 +0800

    update0722

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset' // Vue 项目默认预设
+  ],
+  plugins: [
+    '@babel/plugin-transform-private-methods' // 添加这行
   ]
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true, // 继续转译所有依赖
+  chainWebpack: config => {
+    // 让 babel-loader 排除 pdfjs-dist
+    config.module
+      .rule('js')
+      .exclude
+        .add(/node_modules[\\/]pdfjs-dist/)
+        .end()
+  }
 })
